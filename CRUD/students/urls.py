4f364182577bc2d74ee33fe4915d34f42c3ffddd
Alyssa Batula <batulaa@drexel.edu>
2017-09-28 10:00:41 -0400

    Initial REST implementation

--- CRUD/students/urls.py
+++ CRUD/students/urls.py
@@ -1,9 +1,14 @@
-from django.conf.urls import url
-
+from django.conf.urls import url, include
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'students', views.StudentViewSet)
 
 app_name = 'students'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<student_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- CRUD/students/views.py
+++ CRUD/students/views.py
@@ -1,5 +1,7 @@
 from .models import Student
 from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
+from .serializers import StudentSerializer
 
 
 def index(request):
@@ -11,3 +13,11 @@ def index(request):
 def detail(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
     return render(request, 'students/detail.html', {'student': student})
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows students to be viewed or edited.
+    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
