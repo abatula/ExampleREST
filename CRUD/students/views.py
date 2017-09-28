f4502a53bd4f5195ef5a571efe7501ab77541f53
Alyssa Batula <batulaa@drexel.edu>
2017-09-28 10:33:12 -0400

    Use REST with Django views

--- CRUD/students/views.py
+++ CRUD/students/views.py
@@ -2,22 +2,52 @@ from .models import Student
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from .serializers import StudentSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 
-def index(request):
-    student_list = Student.objects.all()
-    context = {'student_list': student_list}
-    return render(request, 'students/index.html', context)
-
-
-def detail(request, student_id):
-    student = get_object_or_404(Student, pk=student_id)
-    return render(request, 'students/detail.html', {'student': student})
-
-
-class StudentViewSet(viewsets.ModelViewSet):
+@csrf_exempt
+def student_list(request):
     """
-    API endpoint that allows students to be viewed or edited.
+    List all students, or create a new student.
     """
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = StudentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def student_detail(request, student_id):
+    """
+    Retrieve, update or delete a student.
+    """
+    try:
+        student = Student.objects.get(pk=student_id)
+    except Student.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = StudentSerializer(student, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        student.delete()
+        return HttpResponse(status=204)
